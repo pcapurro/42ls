e6a314dbe52f9a8ac1954e637629535034d95544
pcapurro <fossa634@gmail.com>
2025-01-12 22:34:46 +0100

    removed previous main structure of list(), added getFolderElement()

--- srcs/heart.c
+++ srcs/heart.c
@@ -30,68 +30,46 @@ void	printElement(tInfos* infos, char* path)
 		writeStr("\n", 1);
 }
 
+char**	getFolderElements(tInfos* infos, DIR* directory, char* originalPath)
+{
+	t_dirent*	dirEntry;
+	char**		newElements = NULL;
+	char*		element = NULL;
+
+	while (1)
+	{
+		dirEntry = readdir(directory);
+		if (dirEntry == NULL)
+			break ;
+
+		if (infos->hidden == false \
+			&& dirEntry->d_name[0] == '.')
+			continue ;
+
+		if (dirEntry->d_type == DT_DIR)
+			element = getJoin(originalPath, dirEntry->d_name, "/");
+		else
+			element = getJoin(originalPath, dirEntry->d_name, "\0");
+		addElement(&newElements, element);
+	}
+
+	return (newElements);
+}
+
 void	list(tInfos* infos, char** paths)
 {
 	DIR*		directory;
-	t_dirent*	dirEntry;
 	char**		newPaths = NULL;
-	char*		element = NULL;
 
 	for (int i = 0; paths[i] != NULL; i++)
 	{
 		directory = opendir(paths[i]);
 		if (directory == NULL)
-			{ printListError(paths[i]); continue ; }
+			printListError(paths[i]);
 
 		if (getArrLen(paths) > 1 || infos->recursive == true)
 			writeStr(paths[i], 1), writeStr(":\n", 1);
 
-		while (1)
-		{
-			dirEntry = readdir(directory);
-			if (dirEntry == NULL)
-				break ;
-
-			if (infos->hidden == false \
-				&& dirEntry->d_name[0] == '.')
-				continue ;
-
-			if (dirEntry->d_type == DT_DIR)
-				element = getJoin(paths[i], dirEntry->d_name, "/");
-			else
-				element = getJoin(paths[i], dirEntry->d_name, "\0");
-			addElement(&newPaths, element);
-		}
-
-		if (newPaths == NULL)
-			{ printf("\n"); continue ; }
-
-		if (infos->time == true)
-			orderByTime(infos, &newPaths);
-		else
-			orderByAlph(infos, &newPaths);
-
-		for (int k = 0; newPaths[k] != NULL; k++)
-			printElement(infos, newPaths[k]);
-		printf("\n\n");
-
-		if (infos->recursive == true)
-		{
-			for (int k = 0; newPaths[k] != NULL; k++)
-			{
-				if (newPaths[k][getStrLen(newPaths[k]) - 1] == '/')
-					list(infos, newPaths + k);
-			}
-		}
-		newPaths = NULL;
-
-		if (infos->recursive == true \
-			&& paths[i][getStrLen(paths[i]) - 1] == '/')
-			return ;
-
-		if (infos->listing == false)
-			writeStr("\n", 1);
-		if (getArrLen(paths) > 1 && paths[i + 1] != NULL)
-			writeStr("\n", 1);
+		newPaths = getFolderElements(infos, directory, paths[i]);
 	}
 }
